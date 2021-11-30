3cc922c70ab4e50805b5089986420c5b8b0a3770
nasongwut <nasongwut@gmail.com>
2021-11-30 10:45:31 +0700

    Add files via upload

---
+++ main2.py
@@ -0,0 +1,72 @@
+import websocket,serial,base64,struct,time,json
+import _thread,base64
+
+FRAME_SYNC = b'\x55'
+STOP_SYNC = b'\x2e'
+SERIAL_PORT = serial.Serial(
+    port="COM21",
+    baudrate=115200,
+    bytesize=serial.EIGHTBITS,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    timeout=0.1, 
+)
+time.sleep(1)
+
+def get_serial():
+    size = struct.calcsize('iiiiiiiiiii')
+    payload =''
+    msg =  SERIAL_PORT.read()
+    if msg == FRAME_SYNC:
+        msg =  SERIAL_PORT.read(size)    
+        payload = base64.b64encode(msg).decode('ascii')
+    return payload
+
+
+
+def post_serial():
+    START = 85
+    BYTESTOP = 90
+    values = bytearray([255, 2, 3, 4, 5])
+    SERIAL_PORT.write(bytes([START]))
+    for i in values:
+        SERIAL_PORT.write(bytes([i]))
+        time.sleep(0.001)
+    # print('----------------SEND----------------')
+    
+    return
+
+def on_message(ws, message):
+    # print('---------------------income msg'--------------------')
+    pass #print(message)
+
+def on_error(ws, error):
+    pass #print(error)
+
+def on_close(ws, close_status_code, close_msg):
+    pass #print("### closed ###")
+
+def on_open(ws):
+    def run(*args):
+        while True:
+            post_serial()
+            sta_state = get_serial()
+            if sta_state != "":
+                msg = {'type':'Station','bin':sta_state}
+                packet = json.dumps(msg)
+                # print(packet)
+                ws.send(packet)
+                time.sleep(0.001)
+        time.sleep(1)
+        ws.close()
+        print("thread terminating...")
+    _thread.start_new_thread(run, ())
+
+if __name__ == "__main__":
+    # websocket.enableTrace(True)
+    ws = websocket.WebSocketApp("ws://localhost:8000/ws/stationHarn",
+                              on_open=on_open,
+                              on_message=on_message,
+                              on_error=on_error,
+                              on_close=on_close)
+    ws.run_forever()
